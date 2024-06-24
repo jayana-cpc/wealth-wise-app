c075f61b16881c588e5e62df1d9429cfe142f2b8
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-06-23 18:40:17 -0700

    Flask backend setup with tester comp.

---
+++ src/components/flaskTester.js
@@ -0,0 +1,26 @@
+// src/components/ApiComponent.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ApiComponent = () => {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/api/data')
+            .then(response => {
+                setData(response.data.message);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the data!', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Flask API Response:</h1>
+            <p>{data}</p>
+        </div>
+    );
+};
+
+export default ApiComponent;
