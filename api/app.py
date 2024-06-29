7ae998a48ecf895aa8ba710158d8bd69180e54ab
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-06-28 22:36:19 -0700

    Transition changes

--- api/app.py
+++ api/app.py
@@ -1,16 +1,15 @@
-# app.py
+import os
 from flask import Flask, request, jsonify
-
 from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, auth
-import os
 
 app = Flask(__name__)
 CORS(app)
 
-# cred_path = os.path.join(os.path.dirname(__file__), 'secrets\creds.json')
-cred = credentials.Certificate('secrets/creds.json')
+# Use an absolute path or ensure the relative path is correct
+cred_path = os.path.join(os.path.dirname(__file__), 'secrets/wealthwise-46f60-firebase-adminsdk-ykq1h-8f0adf534c.json')
+cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
 
 @app.route('/api/secure-data', methods=['GET'])
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,113 +1,35 @@
-// src/app/login/page.js
-"use client";
+// src/lib/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-import { useRouter } from 'next/navigation';
-import { signInWithGoogle } from '@/lib/firebase';
-import { useToggle, upperFirst } from '@mantine/hooks';
-import { useForm } from '@mantine/form';
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
-import { Paper, Group, TextInput, PasswordInput, Checkbox, Button, Title, Divider, Stack, Anchor } from '@mantine/core';
-import { GoogleButton } from '@/components/buttons/GoogleButton';
-import { useUser } from '@/context/UserContext';
-import classes from './page.module.css';
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export default function AuthenticationImage() {
-  const router = useRouter(); 
-  const { setUser } = useUser();
-  const [type, toggle] = useToggle(['login', 'register']);
-  const form = useForm({
-    initialValues: {
-      email: '',
-      name: '',
-      password: '',
-      terms: true,
-    },
-    validate: {
-      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-      password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
-    },
-  });
+const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    return { token, user };
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData?.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.error('Error during Google sign-in:', { errorCode, errorMessage, email, credential });
+    throw error;
+  }
+};
 
-  const handleLogin = async () => {
-    try {
-      const { user } = await signInWithGoogle();
-      const userData = {
-        photoURL: user.photoURL,
-        displayName: user.displayName,
-        email: user.email,
-      };
-      setUser(userData); 
-      router.push('/dashboard'); 
-    } catch (error) {
-      console.error("Login failed", error);
-    }
-  };
-
-  const handleSubmit = (values) => {
-    console.log(values);
-    router.push('/dashboard');
-  };
-
-  return (
-    <div className={classes.wrapper}>
-      <div className={classes.overlay}></div>
-      <Paper className={classes.form} radius={0} p={30}>
-        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
-          Welcome to Wealth Wise!
-        </Title>
-        <Group grow mb="md" mt="md">
-          <GoogleButton radius="xl" className={classes.button} onClick={handleLogin}>Login with Google</GoogleButton>
-        </Group>
-        <Divider label="Or continue with email" labelPosition="center" my="lg" />
-        <form onSubmit={form.onSubmit(handleSubmit)}>
-          <Stack>
-            {type === 'register' && (
-              <TextInput
-                label="Name"
-                placeholder="Your name"
-                value={form.values.name}
-                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
-                radius="md"
-              />
-            )}
-            <TextInput
-              required
-              label="Email address"
-              placeholder="hello@gmail.com"
-              value={form.values.email}
-              onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-              error={form.errors.email && 'Invalid email'}
-              radius="md"
-            />
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              value={form.values.password}
-              onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-              error={form.errors.password && 'Password should include at least 6 characters'}
-              radius="md"
-            />
-            {type === 'register' && (
-              <Checkbox
-                label="I accept terms and conditions"
-                checked={form.values.terms}
-                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
-              />
-            )}
-          </Stack>
-          <Group justify="space-between" mt="xl">
-            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-              {type === 'register'
-                ? 'Already have an account? Login'
-                : "Don't have an account? Register"}
-            </Anchor>
-            <Button type="submit" radius="xl" className={classes.button}>
-              {upperFirst(type)}
-            </Button>
-          </Group>
-        </form>
-      </Paper>
-    </div>
-  );
-}
+export { auth, signInWithGoogle };
