7752b5c683c7bad465cd3919fc9e3e753b18f607
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-07-19 13:01:11 -0700

    more condensing

--- api/app.py
+++ api/app.py
@@ -4,18 +4,120 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from firebase_admin import credentials, auth, db
 import jwt
+import firebase_admin
+from time import time
+from gemini import Gemini
 
-from utils import User, init_curs, agg_vals, agg_vals_login, BardAI
+# Configuration
+DATABASE_URL = 'https://wealthwise-46f60-default-rtdb.firebaseio.com/'
+cred_path = os.path.join(os.path.dirname(__file__), '..', 'secrets', 'wealthwise-46f60-firebase-adminsdk-ykq1h-2d1bfacc0a.json')
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': DATABASE_URL
+})
+
+SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
 
 app = Flask(__name__)
 CORS(app)
 
-# Configuration
-SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
+class User:
+    def __init__(self, data):
+        self.icon = data['photoURL']
+        self.username = data['displayName']
+        self.email = data['email']
+        self._uid = data['uid']
+        self.regdate = time()
+        self._portfolio = {}
+
+    def post_portfolio_info(self, portfolio):
+        ref = db.reference('users')
+        data = ref.child(f'{self._uid}/portfolio').get()
+        if data is None:
+            data = portfolio
+        else:   
+            data.update(portfolio)
+        ref.child(self._uid).update({
+            'portfolio': data
+        })
+        self._portfolio = data
+
+    def delete_portfolio_info(self, ticker):
+        users_ref = db.reference('users')
+        users_ref.child(f'{self._uid}/portfolio/{ticker}').delete()
+        self._portfolio = users_ref.child(f"{self._uid}/portfolio").get()
+
+    def get_portfolio_info(self):
+        users_ref = db.reference('users')
+        self._portfolio = users_ref.child(f"{self._uid}/portfolio").get()
+        return self._portfolio
+
+    def reg_user(self):
+        ref = db.reference('users')
+        try:
+            if not ref.child(self._uid).get():
+                ref.child(self._uid).set({
+                    "photoURL": self.icon,
+                    "username": self.username,
+                    "email": self.email,
+                    'regdate': self.regdate,
+                })
+                return True, 201
+            return True, 200
+        except Exception as e:
+            print(f"Exception: {e}")
+            return False, 400 
+
+class BardAI:
+    def __init__(self):
+        cookies = {
+            "_ga":"GA1.1.433599624.1712448754",
+            "AEC":"AQTF6HwWYk8cHD8KtVQfQW9jcmN0wfC_82W1bLuoZHH7R7fU22TcHWdI7g",
+            "SID":"g.a000kQjePJ8ZwgvSdXaY7x_wpNDNUoqM_czUrDdf-_0VV_phk2HxA5aPEnv2H96SyJbyv2jRwgACgYKASUSAQASFQHGX2MiPoPzMMsADQlcSj-AHOBpaRoVAUF8yKrCqj_rdLhGZ8OGemCgmLEn0076",
+            "__Secure-1PSID":"g.a000kQjePJ8ZwgvSdXaY7x_wpNDNUoqM_czUrDdf-_0VV_phk2HxnwsqFysKUxyDKNyqYax4WAACgYKAewSAQASFQHGX2MinYNWIqeQsP6Cxb3rFmf9pRoVAUF8yKrCqj_rdLhGZ8OGemCgmLEn0076",
+            "__Secure-3PSID":"g.a000kQjePJ8ZwgvSdXaY7x_wpNDNUoqM_czUrDdf-_0VV_phk2HxaLGH8jrGtri9Bh4xB34yugACgYKARwSAQASFQHGX2Mi5iy_fy9GCoYXp8Lk_rrCSRoVAUF8yKobGwewHsPRa4IBCimtAgpo0076",
+            "HSID":"AH2mKYYudKGBPuVgu",
+            "SSID":"AIICINJIrKl_G6XKy",
+            "APISID":"ph4-0K8Dz-mVzLi3/AXOyqUOEE9fwtbAFr",
+            "SAPISID":"mX_zZMmgxRHrjSb1/AAHFig4THILa9XFXU",
+            "__Secure-1PAPISID":"mX_zZMmgxRHrjSb1/AAHFig4THILa9XFXU",
+            "__Secure-3PAPISID":"mX_zZMmgxRHrjSb1/AAHFig4THILa9XFXU",
+            "_ga_WC57KJ50ZZ":"GS1.1.1717646844.4.1.1717646891.0.0.0",
+            "__Secure-1PSIDTS":"sidts-CjIB3EgAEhm7hUrZGS4R9DPrWuZsD0M0cVmEP2kQ8-Nmikj2Pc409gBjJWG8K0W3wzYecxAA",
+            "__Secure-3PSIDTS":"sidts-CjIB3EgAEhm7hUrZGS4R9DPrWuZsD0M0cVmEP2kQ8-Nmikj2Pc409gBjJWG8K0W3wzYecxAA",
+            "NID":"515",
+            "SIDCC":"AKEyXzXW0P1NeN_Ckg_KVbCVDfYCZCytO9stmHyqVM6cbUem0mgH0LR3sYMooUWmA5ldcfVtBLY",
+            "__Secure-1PSIDCC":"AKEyXzWiwBmmWqwPQ3YckLIb_QY84UD6QhSUGjyIbQtf2vDbpqCn3dR5d9PRD33zuVXa79AlxNM",
+            "__Secure-3PSIDCC":"AKEyXzXqt8P8HbUUrN8mmLzYZiOzHZZE3yyMqBbzPENUeJpd8pfI8OK89eI5vIs3Zn7WzJov5eE"
+        }
+        self.bard = Gemini(cookies=cookies)
+
+    def get_response(self, query):
+        response = self.bard.generate_content(query)
+        return response.text
+
+def init_cors():
+    response = make_response()
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET')
+    response.headers.add('Access-Control-Allow-Methods', 'POST')
+
+def agg_vals(data):
+    email = data.get("email")
+    pwd = data.get('password')
+    fname = data.get('fname')
+    lname = data.get('lname')
+    return email, pwd, fname, lname
+
+def agg_vals_login(data):
+    email = data.get("email")
+    pwd = data.get('password')
+    return email, pwd
 
 @app.before_request
 def before_request():
-    init_curs()
+    init_cors()
 
 @app.route('/api/secure-data', methods=['GET'])
 def secure_data():
@@ -30,8 +132,6 @@ def secure_data():
         return jsonify({'message': 'Secure data', 'uid': uid}), 200
     except Exception as e:
         return jsonify({'message': 'Invalid token', 'error': str(e)}), 401
-
-
 
 @app.route("/api/get-login", methods=['OPTIONS', 'GET'])
 def get_login():
@@ -91,7 +191,6 @@ def get_portfolio_info():
     portfolio_info = user.get_portfolio_info()
     return jsonify(portfolio_info)
 
-
 @app.route("/api/delete-portfolio-info", methods=["POST"])
 def delete_portfolio_info():
     data = request.json
@@ -121,8 +220,6 @@ def get_answer():
     )
 
     return jsonify({'answer': json.dumps(answer, cls=SetEncoder)})
-
-
 
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
