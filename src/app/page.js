5bafc302e6abfb9570c01658a197f52462ae8dde
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-07-20 14:57:27 -0700

    h

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,8 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { gapi } from "gapi-script";
 
-gapi.load("client:auth2", () => {
-  gapi.client.init({
-    clientId:
-      "*****.apps.googleusercontent.com",
-    plugin_name: "chat",
-  });
-});
+
 export default function Home() {
   const router = useRouter();
 
