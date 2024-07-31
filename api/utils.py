14bc2aaf85903ddd6d5f8b28b5db68331d6b8e7d
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-07-31 16:34:55 -0700

    Hidden firebase env

--- api/utils.py
+++ api/utils.py
@@ -37,8 +37,22 @@ from gemini import Gemini
 # Authenticate Firebase, Establish Connection
 # Use an absolute path or ensure the relative path is correct
 DATABASE_URL = 'https://wealthwise-46f60-default-rtdb.firebaseio.com/'
-cred_path = os.path.join(os.path.dirname(__file__), 'secrets/wealthwise-46f60-firebase-adminsdk-ykq1h-e91bf8f8d1.json')
-cred = credentials.Certificate(cred_path)
+
+# Load credentials from environment variables
+cred = credentials.Certificate({
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+})
+
 firebase_admin.initialize_app(cred, {
     'databaseURL': DATABASE_URL
 })
