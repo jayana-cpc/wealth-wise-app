3abaffe7051818099379d22b9bab70b9c66aceed
Jayana Nanayakkara <jchdnjc@gmail.com>
2024-08-01 13:05:35 -0700

    Fixed linting issues

--- src/app/learn/stockValuation/page.js
+++ src/app/learn/stockValuation/page.js
@@ -1,44 +1,8 @@
 "use client";
 import { ActionsGrid } from "@/components/Learn/StockValuationHome/ActionGrid";
-import { HeaderSimple } from "@/components/Header/HeaderSimple";
-import { Stepper, Center, Space } from "@mantine/core";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { NavBarTemplate } from "@/components/Navbar/NavBarTemplate";
 import { HeroImageBackground } from "@/components/Header/CourseHeader";
 export default function Dashboard() {
-  const router = useRouter();
-  const [hoveredStep, setHoveredStep] = useState(-1);
-
-  const navigateToStep = (step) => {
-    switch (step) {
-      case 1:
-        router.push("/learn/stockValuation/understandingBusiness");
-        break;
-      case 2:
-        router.push("/learn/stockValuation/relativeValuationIntro");
-        break;
-      case 3:
-        router.push("/learn/stockValuation/enterpriseValueMultiples");
-        break;
-      case 4:
-        router.push("/learn/stockValuation/equityValueMultiples");
-        break;
-      case 5:
-        router.push("/learn/stockValuation/discountedCashflowValuation");
-        break;
-      default:
-        router.push("/learn/stockValuation/stockSelect");
-    }
-  };
-
-  const handleMouseEnter = (step) => {
-    setHoveredStep(step);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredStep(-1);
-  };
   return (
     <div>
       <NavBarTemplate>
@@ -53,10 +17,8 @@ export default function Dashboard() {
             addition to your portfolio."
         />
         <ActionsGrid />
-
-        
-        
       </NavBarTemplate>
     </div>
   );
 }
+
