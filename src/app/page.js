49f5bdcfb70ac7a1051974645b65242a3c6723ad
Rahul P <rahul.puritipati@gmail.com>
2024-08-09 22:07:55 -0700

    pls work

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
+export default function Page() {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      if (window.gtag) {
-        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-          page_path: url,
-        });
-      }
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
     };
 
     // Track the initial page load
     handleRouteChange(window.location.pathname);
 
-    // Track subsequent route changes
+    // Track route changes
     router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [router]);
-
-  useEffect(() => {
-    router.push("/login");
   }, [router]);
 
   return null;
