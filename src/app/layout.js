301d0aac449f93ae3b5b39e546f7cf61c1b6b11e
Rahul P <rahul.puritipati@gmail.com>
2024-08-09 21:57:59 -0700

    dddd

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,9 @@ import "@mantine/core/styles.css";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { UserProvider } from "@/context/UserContext";
 import { NavbarProvider } from "@/context/NavBarContext";
-import Script from 'next/script';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+
 
 import "public/styles/globals.css"; // Import global styles
-
 export const metadata = {
   title: "Wealth Wise",
   description: "Build Financial Literacy NOW",
@@ -17,51 +14,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      if (window.gtag) {
-        window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-          page_path: url,
-        });
-      }
-    };
-
-    // Track the initial page load
-    handleRouteChange(window.location.pathname);
-
-    // Track subsequent route changes
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router]);
 
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </head>
       <body>
         <UserProvider>
