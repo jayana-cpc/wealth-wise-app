302fc410621e6b869f0c81ce0b9d819381f63ef2
Rahul P <rahul.puritipati@gmail.com>
2024-11-20 19:44:44 -0800

    markdown

--- src/components/PortfolioAdvisorProps/portfolioAnalysisProp.js
+++ src/components/PortfolioAdvisorProps/portfolioAnalysisProp.js
@@ -132,6 +132,7 @@ export function PortfolioAnalysisProp() {
 
     try {
       const response = await fetch("https://wealthwize.app/api/get-answer", {
+        // const response = await fetch("http://localhost:5000/api/get-answer", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
